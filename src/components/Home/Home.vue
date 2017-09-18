f8a7b5c7227a4ab81c642b5d2da14fb8e4a9f8e5
CC13121339436 <13121339436@163.com>
2017-09-18 21:49:01 +0800

    add Home Fotter del src>node

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer></v-footer>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/public/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="footer">
+        <ul>
+            <li>
+                <router-link to="">首页</router-link>
+            </li>
+            <li>
+                <router-link to="">分类</router-link>
+            </li>
+            <li>
+                <router-link to="">购物车</router-link>
+            </li>
+            <li>
+                <router-link to="">我的</router-link>
+            </li>
+            <li>
+                <router-link to="">更多</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default{
+        name : "footer"
+    }
+</script>
+
+<style>
+    .footer {position:fixed; bottom:0; left: 0; width: 100%; height: 50px; border-top: 1px solid #ccc; line-height: 50px; text-align: center; background:#fff;border-top:1px solid #ccc;z-index: 1;}
+    .footer ul { overflow:hidden;}  
+    .footer li { float:left; width:20%}
+</style>
+
